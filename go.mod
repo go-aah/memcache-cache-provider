1cc58dfb7c09f8eff5af4c09b8ec897b66107e06
Jeevanandam M <jeeva@myjeeva.com>
2018-10-13 00:36:04 -0700

    memcache cache provider updated to go mod

---
+++ go.mod
@@ -0,0 +1,7 @@
+module aahframe.work/cache/provider/memcache
+
+require (
+	aahframe.work v0.0.0-20181013054310-9873edb08c38
+	github.com/bradfitz/gomemcache v0.0.0-20180710155616-bc664df96737
+	github.com/stretchr/testify v1.2.2
+)
--- memcache.go
+++ memcache.go
@@ -12,9 +12,9 @@ import (
 	"sync"
 	"time"
 
-	"aahframe.work/aah/cache"
-	"aahframe.work/aah/config"
-	"aahframe.work/aah/log"
+	"aahframe.work/cache"
+	"aahframe.work/config"
+	"aahframe.work/log"
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
